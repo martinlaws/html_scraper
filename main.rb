4578ae38793c130f70d2747a7377481301d4a817
Martin Laws <martinblaws@gmail.com>
2015-10-29 00:12:31 +0000

    Colorized output using 'colorize' ruby gem

--- main.rb
+++ main.rb
@@ -1,17 +1,20 @@
 require 'nokogiri'
 require 'pry'
+require 'open-uri'
+require 'colorize'
 require_relative './comment.rb'
 
 # PAGE_URL = "https://news.ycombinator.com/item?id=7663775"
-PAGE_URL = "./post.html"
+# PAGE_URL = "./post.html"
+HTML_FILE = open(ARGV[0])
 
 class Post
   attr_accessor :new_comments
-  @@doc = Nokogiri::HTML(File.open(PAGE_URL))
+  @@doc = Nokogiri::HTML(HTML_FILE)
 
   def initialize
     @title   = (@@doc.css('head title').map { |title| title.inner_text }).join
-    @url     = PAGE_URL
+    @url     = HTML_FILE
     @points  = (@@doc.search('.subtext > span:first-child').map { |span| span.inner_text}).first
     @item_id = (@@doc.search('.subtext > a:nth-child(3)'))[0]["href"]
     @comment_array = []
@@ -23,7 +26,10 @@ class Post
 
   def comments
     # returns all comments
-    @comment_array.each { |comment_object| puts comment_object.text }
+    @comment_array.each do |comment_object| 
+      puts (comment_object.text).colorize(:light_blue)
+      puts '~~~~~'.colorize(:red)
+    end
     puts
   end
 
@@ -38,7 +44,8 @@ end
 #   end
 # end
 
+
+
 p1 = Post.new
 p1.comments
-p1.add_comment("TEST COMMENT")
-p1.comments
+
